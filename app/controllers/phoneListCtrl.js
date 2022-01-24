01633a81a37af8d2deeeee9632c25b9607ee39a3
Kelvis Fernando <kelvisdelapena@gmail.com>
2022-01-24 20:07:47 -0300

    Adicionado validacao de phone

--- app/controllers/phoneListCtrl.js
+++ app/controllers/phoneListCtrl.js
@@ -1,19 +1,24 @@
 angular.module("phoneList").controller("phoneListCtrl", function ($scope) {
   $scope.contacts = [];
   $scope.createContact = function () {
-    $scope.contacts.push({
-      id: $scope.index,
-      name: $scope.name,
-      phone: $scope.phone,
-    });
+    if ($scope.phone.length < 8 || $scope.phone.length > 9) {
+      alert("Adicione um numero Valido");
+    } else {
+      $scope.contacts.push({
+        id: $scope.index,
+        name: $scope.name,
+        phone: $scope.phone,
+      });
 
-    $scope.name = "";
-    $scope.phone = "";
+      $scope.name = "";
+      $scope.phone = "";
+    }
   };
 
   $scope.deleteContact = function (name) {
     $scope.contacts.splice(
-      $scope.contacts.findIndex((index) => index.name === name),1
+      $scope.contacts.findIndex((index) => index.name === name),
+      1
     );
   };
 });
