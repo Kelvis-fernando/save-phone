c0df64fec86d7585f66cb17616e50367ae919df4
Kelvis Fernando <kelvisdelapena@gmail.com>
2022-01-26 09:24:54 -0300

    Adicionado o campo de filtro, e feito o filtro de nomes com o filter

--- app/controllers/phoneListCtrl.js
+++ app/controllers/phoneListCtrl.js
@@ -1,24 +1,26 @@
-angular.module("phoneList").controller("phoneListCtrl", function ($scope) {
-  $scope.contacts = [];
-  $scope.createContact = function () {
-    if ($scope.phone.length < 8 || $scope.phone.length > 9) {
-      alert("Adicione um numero Valido");
-    } else {
-      $scope.contacts.push({
-        id: $scope.index,
-        name: $scope.name,
-        phone: $scope.phone,
-      });
+angular.module("phoneList")
+  .controller("phoneListCtrl", function ($scope) {
+    $scope.contacts = [];
+    $scope.createContact = function () {
+      if ($scope.phone.length < 8 || $scope.phone.length > 9) {
+        alert("Adicione um numero Valido");
+      } else {
+        $scope.contacts.push({
+          id: $scope.index,
+          name: $scope.name,
+          phone: $scope.phone,
+          date: new Date(),
+        });
 
-      $scope.name = "";
-      $scope.phone = "";
-    }
-  };
+        $scope.name = "";
+        $scope.phone = "";
+      }
+    };
 
-  $scope.deleteContact = function (name) {
-    $scope.contacts.splice(
-      $scope.contacts.findIndex((index) => index.name === name),
-      1
-    );
-  };
-});
+    $scope.deleteContact = function (name) {
+      $scope.contacts.splice(
+        $scope.contacts.findIndex((index) => index.name === name),
+        1
+      );
+    };
+  });
